56df0cd6c8d2d27b64ec69eed436d037de23f320
theocbr <theo.carbonnier@hotmail.fr>
2022-04-21 13:15:14 +0200

    tyhtyh

--- ressources.php
+++ ressources.php
@@ -154,7 +154,6 @@ function masquer_div(id)
             </ul>
             </div>
             <div id="search">
-            <label id="labelmenufoot">Rechercher :</label><br />
             <br>
             <ion-icon id="iconfootsearch" name="search-circle-outline"></ion-icon><input type="search" id="search" placeholder="Search...." required><button id="btnrechercher" >Rechercher</button>
             </div>
