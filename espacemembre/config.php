071f668c937eb15761b9ded4eea6346d566dcb4c
theocbr <theo.carbonnier@hotmail.fr>
2022-09-14 16:19:33 +0200

    up

--- espacemembre/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// Connexion à la base de donnée , on indique la table .
-try{
-    $bdd = new PDO('mysql:host=localhost;dbname=forum;charset=utf8','root','');
-}catch(Exception $e){
-    die('Erreur'.$e->getMessage());
-}
-
-?>
--- index.php
+++ index.php
@@ -19,7 +19,27 @@ function masquer_div(id)
   }
   
 }
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+     
+    FB.AppEvents.logPageView();  
+     
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 </script>
+
 <!DOCTYPE html>
 <html lang="fr">
   <html>
